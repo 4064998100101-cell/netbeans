3db96e79a96297ee0f1a637cb61ddda34c9aba0f
Santiago Benjamin Tzoc Ican <4064998100101@ingenieria.usac.edu.gt>
2026-06-04 10:17:52 -0600

    Ejercicio del 4-06-2026
    Aqui se encuentra el ejercicio del dia

---
+++ programas/Ejemplo1_2clase4/src/main/java/com/mycompany/ejemplo1_2clase4/Ejemplo1_2clase4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemplo1_2clase4;
+
+/**
+ *
+ * @author 5toco
+ */
+public class Ejemplo1_2clase4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
