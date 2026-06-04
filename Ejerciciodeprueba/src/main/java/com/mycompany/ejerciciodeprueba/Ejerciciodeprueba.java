19128aae32d1ea3f0cb131da5ba85d2bce5066a7
Santiago Benjamin Tzoc Ican <4064998100101@ingenieria.usac.edu.gt>
2026-06-03 21:19:02 -0600

    Datos y cambios
    Agregamos mas codigo al programa del rombo

--- Ejerciciodeprueba/src/main/java/com/mycompany/ejerciciodeprueba/Ejerciciodeprueba.java
+++ Ejerciciodeprueba/src/main/java/com/mycompany/ejerciciodeprueba/Ejerciciodeprueba.java
@@ -1,18 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.ejerciciodeprueba;
- import java.util.Scanner;
-/**
- *
- * @author 5toco
- */
+
+import java.util.Scanner;
+
 public class Ejerciciodeprueba {
 
     public static void main(int N) {
+      Scanner leer = new Scanner(System.in); 
+//Ingresamos el valor del rombo que necesitamos
       System.out.println("Ingrese un numero impar positivo");
-        
+      N= leer.nextInt(); 
+      
+      for (int i=1; i<=N ;i++ )
+      {
+      //Definir los espacios del lado de arriba
+      
+      
+      }
         
     }
 }
