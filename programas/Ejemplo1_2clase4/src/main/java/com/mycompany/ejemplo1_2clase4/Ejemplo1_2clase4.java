600b8f96ed820c5c083102c11054a586cd41dd1f
Santiago Benjamin Tzoc Ican <4064998100101@ingenieria.usac.edu.gt>
2026-06-04 11:02:42 -0600

    Serie numerica
    Programa que permite ver la secuencia vista en el enunciado

--- programas/Ejemplo1_2clase4/src/main/java/com/mycompany/ejemplo1_2clase4/Ejemplo1_2clase4.java
+++ programas/Ejemplo1_2clase4/src/main/java/com/mycompany/ejemplo1_2clase4/Ejemplo1_2clase4.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.ejemplo1_2clase4;
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,38 @@ package com.mycompany.ejemplo1_2clase4;
 public class Ejemplo1_2clase4 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      Scanner leer=new Scanner(System.in);
+int n;
+      long numeropar=5;
+      long numeroimpar=3; 
+
+      //Realizar un programa que respete el patron seguido
+      System.out.println("--------Programa de secuencia");
+      System.out.println("Ingrese el valor n que desea encontrar");
+      n=leer.nextInt();
+      System.out.println("1 = "+numeroimpar);
+      System.out.println("2 = "+numeropar);
+      for (int i=3;i<=n;i++)
+      
+      {
+      long nuevopar=0,nuevoimpar=0;
+   
+      if (i%2==0)  
+      {
+      nuevopar=numeropar*3;
+      numeropar=nuevopar;
+      System.out.println(i+" = "+nuevopar);
+      }
+      else{
+      
+      nuevoimpar=numeroimpar*2;
+      
+      numeroimpar=nuevoimpar;
+      System.out.println(i+" = "+nuevoimpar);
+      }
+      
+      }
+      
+    
     }
 }
