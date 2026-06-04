a6eca4867e162f5da597a5520e2bee5bd8fcbe90
Santiago Benjamin Tzoc Ican <4064998100101@ingenieria.usac.edu.gt>
2026-06-03 21:06:39 -0600

    Primeros pasos
    Aqui se encuentra lo primero

---
+++ Ejerciciodeprueba/src/main/java/com/mycompany/ejerciciodeprueba/Ejerciciodeprueba.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejerciciodeprueba;
+ import java.util.Scanner;
+/**
+ *
+ * @author 5toco
+ */
+public class Ejerciciodeprueba {
+
+    public static void main(int N) {
+      System.out.println("Ingrese un numero impar positivo");
+        
+        
+    }
+}
